4ceb9ed4ae38f19cc25e0c6389eaf43bcf8a6218
Charlotte Fritz <charlotte.fritz@mail.utoronto.ca>
2023-11-24 08:43:17 -0500

    multi client

--- client.c
+++ client.c
@@ -1,5 +1,6 @@
 #include <arpa/inet.h> // inet_addr()
 #include <netdb.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,66 +8,101 @@
 #include <sys/socket.h>
 #include <unistd.h> // read(), write(), close()
 #define MAX 80
-#define PORT 8030
+#define PORT 8040
 #define SA struct sockaddr
-void func(int sockfd)
-{
-	char buff[MAX];
-	int n;
-	for (;;) {
-		bzero(buff, sizeof(buff));
-		printf("Enter the string : ");
-		n = 0;
-		while ((buff[n++] = getchar()) != '\n')
-			;
-		write(sockfd, buff, sizeof(buff));
 
-		bzero(buff, sizeof(buff));
-		read(sockfd, buff, sizeof(buff));
+// Wait for user input
+char userInput[100];
 
+bool first = true;
 
+void welcome();
 
+void func(int sockfd) {
+  if (first == false) {
+    char buff[MAX];
+    int n;
+    for (;;) {
+      bzero(buff, sizeof(buff));
+      printf("Enter command : ");
+      n = 0;
+      while ((buff[n++] = getchar()) != '\n')
+        ;
+      write(sockfd, buff, sizeof(buff));
 
-		printf("From Server : %s", buff);
-		if ((strncmp(buff, "quit", 4)) == 0) {
-			printf("Client Quit...\n");
-			break;
-		}
-	}
+      bzero(buff, sizeof(buff));
+      read(sockfd, buff, sizeof(buff));
+
+      printf("From Server : %s", buff);
+      if ((strncmp(buff, "quit", 4)) == 0) {
+        printf("Client Quit...\n");
+        break;
+      }
+    }
+  }
 }
 
-int main()
-{
-	int sockfd, connfd;
-	struct sockaddr_in servaddr, cli;
+int main() {
+  welcome();
+  int sockfd, connfd;
+  struct sockaddr_in servaddr, cli;
 
-	// socket create and verification
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd == -1) {
-		printf("socket creation failed...\n");
-		exit(0);
-	}
-	else
-		printf("Socket successfully created..\n");
-	bzero(&servaddr, sizeof(servaddr));
+  // socket create and verification
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd == -1) {
+    printf("socket creation failed...\n");
+    exit(0);
+  } else
+    // printf("Socket successfully created..\n");
+    bzero(&servaddr, sizeof(servaddr));
 
-	// assign IP, PORT
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	servaddr.sin_port = htons(PORT);
+  // assign IP, PORT
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  servaddr.sin_port = htons(PORT);
 
-	// connect the client socket to server socket
-	if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr))
-		!= 0) {
-		printf("connection with the server failed...\n");
-		exit(0);
-	}
-	else
-		printf("connected to the server..\n");
+  // connect the client socket to server socket
+  if (connect(sockfd, (SA *)&servaddr, sizeof(servaddr)) != 0) {
+    printf("connection with the server failed...\n");
+    exit(0);
+  } else
+    first = false;
+  // printf("connected to the server..\n");
 
-	// function for chat
-	func(sockfd);
+  // function for chat
+  func(sockfd);
 
-	// close the socket
-	close(sockfd);
+  // close the socket
+  close(sockfd);
+}
+
+void welcome() {
+  printf("Welcome to Multi-Party Text Conferencing Program!\n");
+  printf("====================================================================="
+         "============\n");
+  printf("Available Commands:\n");
+  printf("1. /login <username> <password> <IP> <port>    - Log in with your "
+         "credentials\n");
+  printf("2. /logout                                     - Exit the server\n");
+  printf("3. /join <session_id>                          - Join a specific "
+         "session\n");
+  printf("4. /leavesession                               - Leave the current "
+         "session\n");
+  printf(
+      "5. /createsession <session_id>                 - Create a new session "
+      "and join it\n");
+  printf("6. /list                                       - List connected "
+         "clients and available sessions\n");
+  printf("7. /quit                                       - Terminate the "
+         "program\n");
+  printf("8. <text>                                      - Send a message to "
+         "the current conference session\n");
+  printf("====================================================================="
+         "============\n");
+
+  // Prompt the user to enter something
+  printf("Enter a command: ");
+
+  scanf("%99s", userInput);
+  return;
 }
